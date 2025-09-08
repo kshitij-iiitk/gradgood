0959f8264995dd4464ab73e9474388e24be3af6a
Kittu616 <kshitijrphutane@gmail.com>
2025-09-08 22:07:21 +0530

    Testing format-2 version3

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,9 @@ app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
 
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 // Start server
